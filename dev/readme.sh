118b789e6378cea0104840680d2f7a9b47a23117
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2023-10-20 17:28:58 -0300

    ci: add CI pipeline
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

---
+++ dev/readme.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+declare TRACE
+[[ "${TRACE}" == 1 ]] && set -o xtrace
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readme() {
+  cat <<EOF >README.md
+# collatz
+
+collatz is a CLI for printing the [hailstone sequence](https://en.wikipedia.org/wiki/Collatz_conjecture) of a NUMBER.
+
+[![Build status](https://github.com/rodmoioliveira/collatz/workflows/ci/badge.svg)](https://github.com/rodmoioliveira/collatz/actions)
+
+## Installation
+
+Archives of [precompiled binaries](https://github.com/rodmoioliveira/collatz/releases)
+for \`collatz\` are available for Windows, macOS and Linux.
+
+## Building
+
+\`collatz\` is written in Rust, so you'll need to grab a [Rust installation](https://www.rust-lang.org/tools/install)
+in order to compile it. To build \`collatz\`, run:
+
+\`\`\`
+git clone git@github.com:rodmoioliveira/collatz.git
+cd collatz
+make install
+\`\`\`
+
+## Usage
+
+\`\`\`
+cargo run -- --help
+
+$(cargo run -- --help)
+\`\`\`
+EOF
+
+  sd '(make\[1\]:.+\n)' '' README.md
+  sd 'cargo run --' 'collatz' README.md
+}
+
+trap readme EXIT
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
 use clap::Parser;
 
-#[derive(Debug, Parser)]
-#[clap(name = env!("CARGO_PKG_NAME"))]
-#[clap(version = env!("CARGO_PKG_VERSION"))]
-#[clap(author = clap::crate_authors!())]
-#[clap(color = clap::ColorChoice::Never)]
-#[clap(about = clap::crate_description!(), long_about = clap::crate_description!())]
-pub struct Cli {
-    #[clap(
-        required = true,
-        help = "A positive integer between 0 and u32::MAX",
-        long_help = "A positive integer between 0 and 4294967295",
-    )]
-    pub number: u32,
-}
-
 // The Rust standard library suppresses the default SIGPIPE behavior, so that
 // writing to a closed pipe doesn't kill the process.
 //
@@ -30,29 +15,11 @@ fn reset_sigpipe() {
 
 fn main() {
     reset_sigpipe();
-    let args = Cli::parse();
+    let args = collatz::cli::Cli::parse();
 
-    if let 0..=1 = args.number {
-        eprintln!("no steps");
-        std::process::exit(0);
-    };
-
-    let mut next = args.number as u128;
-    let mut step = 0;
-
-    println!("next step");
-    while next != 1 {
-        step += 1;
-
-        match next % 2 == 0 {
-            true => {
-                next /= 2;
-            }
-            false => {
-                next = (next * 3) + 1;
-            }
-        }
-
-        println!("{step} {next}");
+    let seq = collatz::collatz::sequence(args.number);
+    println!("step next");
+    for (i, n) in seq.iter().enumerate() {
+        println!("{s} {n}", s = i + 1);
     }
 }
